850f447348d6ac56f895837b477b567dd3b8a62e
Lucas Nunes <lucas.nunesgomes1@gmail.com>
2024-01-23 12:12:34 -0300

    created `Order`, `OrderRepository`, `OrderResource` and `OrderService`

--- src/main/java/com/lucasnunes/webservices/config/TestConfig.java
+++ src/main/java/com/lucasnunes/webservices/config/TestConfig.java
@@ -1,12 +1,15 @@
 package com.lucasnunes.webservices.config;
 
+import com.lucasnunes.webservices.entities.Order;
 import com.lucasnunes.webservices.entities.User;
+import com.lucasnunes.webservices.repositories.OrderRepository;
 import com.lucasnunes.webservices.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
@@ -16,6 +19,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
 
     @Override
     public void run(String... args) throws Exception {
@@ -23,7 +29,11 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "999999", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "888888", "123456");
 
-        userRepository.saveAll(Arrays.asList(u1, u2));
+        Order o1 = new Order(null, Instant.parse("2019-06-20T21:33:17Z"), u1);
+        Order o2 = new Order(null, Instant.parse("2020-07-21T03:42:10Z"), u2);
+        Order o3 = new Order(null, Instant.parse("2020-01-12T21:55:32Z"), u2);
 
+        userRepository.saveAll(Arrays.asList(u1, u2));
+        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
     }
 }
