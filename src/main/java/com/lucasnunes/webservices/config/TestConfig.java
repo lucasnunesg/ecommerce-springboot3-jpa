476cdc0d1339f06c1c4aceeeda58d84297db2505
Lucas Nunes <lucas.nunesgomes1@gmail.com>
2024-01-23 12:25:33 -0300

    implemented `OrderStatus` enum

--- src/main/java/com/lucasnunes/webservices/config/TestConfig.java
+++ src/main/java/com/lucasnunes/webservices/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.lucasnunes.webservices.config;
 
 import com.lucasnunes.webservices.entities.Order;
 import com.lucasnunes.webservices.entities.User;
+import com.lucasnunes.webservices.entities.enums.OrderStatus;
 import com.lucasnunes.webservices.repositories.OrderRepository;
 import com.lucasnunes.webservices.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "999999", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "888888", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T21:33:17Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2020-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2020-01-12T21:55:32Z"), u2);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T21:33:17Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2020-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2020-01-12T21:55:32Z"), OrderStatus.WAITING_PAYMENT, u2);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
